534bebc25a5e2fb91f1e2ea7626c2e7a4cf6d8c9
senthamizhselvan-sm <senthamizhselvan.sm@gmail.com>
2025-11-19 20:55:01 +0530

    status update

--- webserver.js
+++ webserver.js
@@ -3,8 +3,20 @@ const app = express()
 const path = require('path')
 const PORT = process.env.PORT || 3500;
 
-app.get('/' , (req,res) => {
-    res.send('hello world')
+app.get(/^\/$|\/index(.html)?/, (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get(/^\/new-page(\.html)?$/ , (req,res) => {
+    res.sendFile(path.join(__dirname , 'views' , 'new-page.html'));
+});
+
+app.get(/^\/old-page(\.html)?$/ , (req,res) => {
+    res.redirect(301,'/new-page.html');
+});
+
+app.get(/^\/.*$/ , (req,res) => {
+    res.status(404).sendFile(path.join(__dirname , 'views' , '404.html'));
 });
 
 app.listen(PORT , () =>{
