1b67a48512b68b5fbf20ff9f9daf02cda8457cd3
senthamizhselvan-sm <senthamizhselvan.sm@gmail.com>
2025-11-19 19:15:32 +0530

    simple web server creation

---
+++ webserver.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const PORT = process.env.PORT || 3500;
+
+app.get('/' , (req,res) => {
+    res.send('hello world')
+});
+
+app.listen(PORT , () =>{
+    console.log(`server running on port ${PORT}`);
+})
