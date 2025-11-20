9ad19a3261fb6f1715f6ed1c15aea03c0ed3b211
senthamizhselvan-sm <senthamizhselvan.sm@gmail.com>
2025-11-20 20:18:33 +0530

    errorhandling & handling request from other server

---
+++ middleware/errorHandler.js
@@ -0,0 +1,10 @@
+const {logEvents} = require('./logEvents.js')
+
+const errorHandler = (err, req , res , next) =>{
+    logEvents(`${err.name} : ${err.message} ` , 'errLog.txt')
+    console.log(err.stack)
+    res.status(500).send(err.message)
+    next();
+}
+
+module.exports = {errorHandler}
--- webserver.js
+++ webserver.js
@@ -1,10 +1,27 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const cors = require('cors')
 const {logger} = require('./middleware/logEvents.js')
+const {errorHandler} = require('./middleware/errorHandler.js')
 const PORT = process.env.PORT || 3500;
 
 app.use(logger)
+
+const whiteLists = ['https://www.yoursite.com' , 'http://localhost:3500'];
+
+const corsOptions = {
+    origin : (origin , callback) =>{
+        if(whiteLists.indexOf(origin) !== -1 || !origin) {
+            callback(null,true)
+        } else{
+            callback(new Error('not allowed by CORS'))
+        }
+    },
+    OptionssuccessStatus : 200
+}
+app.use(cors(corsOptions))
+
 app.use(express.urlencoded({ extended : false}))
 app.use(express.json())
 app.use(express.static(path.join(__dirname , '/public')));
@@ -49,6 +66,8 @@ app.get(/^\/chain(.html)?$/, [one, two, three]);
 app.get(/^\/.*$/ , (req,res) => {
     res.status(404).sendFile(path.join(__dirname , 'views' , '404.html'));
 });
+
+app.use(errorHandler)
 
 app.listen(PORT , () =>{
     console.log(`server running on port ${PORT}`);
